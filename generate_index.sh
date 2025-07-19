18656c4540fbea22f343d15557d8c94ebdefe5a6
Sanjay <itssanjay@gmail.com>
2025-07-18 23:17:55 -0400

    cleaning apps list

--- generate_index.sh
+++ generate_index.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 # Create temporary files for the new apps lists
-temp_file_general=$(mktemp)
-temp_file_other=$(mktemp)
+temp_file_apps=$(mktemp)
 
 echo "Generating General Apps section..."
 
@@ -14,7 +13,7 @@ for dir in */; do
         if [ -z "$title" ]; then
             title=${dir%/}  # Use directory name if no title found
         fi
-        echo "                        <li><a href=\"/${dir%/}\">${title}</a></li>" >> "$temp_file_general"
+        echo "                        <li><a href=\"/${dir%/}\">${title}</a></li>" >> "$temp_file_apps"
     fi
 done
 
@@ -34,10 +33,11 @@ if [ -f "other-apps-repo.txt" ]; then
         repo_info=$(echo "$repo_url" | sed 's|https://github.com/||')
         username=$(echo "$repo_info" | cut -d'/' -f1)
         repo_name=$(echo "$repo_info" | cut -d'/' -f2)
+        branch=$(echo "$repo_info" | cut -d'/' -f4)
         
         # Try to fetch CNAME file (try main branch first, then master)
         dns_name=""
-        cname_content=$(curl -s "https://raw.githubusercontent.com/$username/$repo_name/main/CNAME" 2>/dev/null)
+        cname_content=$(curl -s "https://raw.githubusercontent.com/$username/$repo_name/$branch/CNAME" 2>/dev/null)
         if [ $? -ne 0 ] || [ -z "$cname_content" ]; then
             cname_content=$(curl -s "https://raw.githubusercontent.com/$username/$repo_name/master/CNAME" 2>/dev/null)
         fi
@@ -46,13 +46,11 @@ if [ -f "other-apps-repo.txt" ]; then
             dns_name=$(echo "$cname_content" | tr -d '\n' | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')
         fi
         
-        # Try to fetch index.html file (try main branch first, then master)
+        # Try to fetch index.html file from the provided URL
         title=""
-        html_content=$(curl -s "https://raw.githubusercontent.com/$username/$repo_name/main/index.html" 2>/dev/null)
-        if [ $? -ne 0 ] || [ -z "$html_content" ] || [ "$html_content" = "404: Not Found" ]; then
-            html_content=$(curl -s "https://raw.githubusercontent.com/$username/$repo_name/master/index.html" 2>/dev/null)
-        fi
-        
+        raw_html_url=$(echo "$repo_url" | sed 's|github.com|raw.githubusercontent.com|' | sed 's|/blob/|/|')
+        html_content=$(curl -sL "$raw_html_url" 2>/dev/null)
+
         if [ -n "$html_content" ] && [ "$html_content" != "404: Not Found" ]; then
             title=$(echo "$html_content" | grep -i "<title>" | sed -e 's/<[^>]*>//g' | tr -d '\n' | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')
         fi
@@ -63,30 +61,36 @@ if [ -f "other-apps-repo.txt" ]; then
         fi
         
         # Use HTTPS URL with DNS name if available, otherwise use GitHub Pages URL
+        path_in_repo=$(echo "$repo_url" | sed -n 's|https://github.com/[^/]*/[^/]*/blob/[^/]*/\(.*\)/index.html|\1|p')
+        
         if [ -n "$dns_name" ]; then
             url="https://$dns_name"
         else
-            url="https://$username.github.io/$repo_name"
+            base_url="https://$username.github.io/$repo_name"
+            if [ -n "$path_in_repo" ]; then
+                url="$base_url/$path_in_repo/"
+            else
+                url="$base_url/"
+            fi
         fi
         
-        echo "                        <li><a href=\"$url\" target=\"_blank\" rel=\"noopener\">$title</a></li>" >> "$temp_file_other"
+        echo "                        <li><a href=\"$url\" target=\"_blank\" rel=\"noopener\">$title</a></li>" >> "$temp_file_apps"
         
     done < "other-apps-repo.txt"
-    
-    # Sort the other apps list
-    sort -o "$temp_file_other" "$temp_file_other"
 fi
 
+# Sort the combined apps list alphabetically by title
+sort_temp_file=$(mktemp)
+sed 's|.*<a.*>\(.*\)</a.*|\1|' "$temp_file_apps" | paste -d'|' - "$temp_file_apps" | sort | cut -d'|' -f2- > "$sort_temp_file"
+mv "$sort_temp_file" "$temp_file_apps"
+
+
 # Replace content between <ol id="pages"> and </ol> tags for General Apps
-sed -i${sed_backup:+' '} -e '/<ol id="pages">/,/<\/ol>/{/<ol id="pages">/!{/<\/ol>/!d}}' -e "/<ol id=\"pages\">/r $temp_file_general" index.html
+sed -i${sed_backup:+' '} -e '/<ol id="pages">/,/<\/ol>/{/<ol id="pages">/!{/<\/ol>/!d}}' -e "/<ol id=\"pages\">/r $temp_file_apps" index.html
 
-# Replace content between <ol id="otherpages"> and </ol> tags for Other Apps
-sed -i${sed_backup:+' '} -e '/<ol id="otherpages">/,/<\/ol>/{/<ol id="otherpages">/!{/<\/ol>/!d}}' -e "/<ol id=\"otherpages\">/r $temp_file_other" index.html
-
-echo "Generated $(wc -l < "$temp_file_general") General Apps"
-echo "Generated $(wc -l < "$temp_file_other") Other Apps"
+echo "Generated $(wc -l < "$temp_file_apps") Apps"
 
 # Clean up
-rm "$temp_file_general" "$temp_file_other"
+rm "$temp_file_apps"
 
 echo "Index generation complete!" 
